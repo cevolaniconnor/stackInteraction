bc4be89780426e65750fee78e0f725377dd0c86d
cevolaniconnor <70539453+cevolaniconnor@users.noreply.github.com>
2024-12-02 16:56:41 -0800

    Added peek, pop, push, isEmpty onto the stack

--- stack.cpp
+++ stack.cpp
@@ -5,10 +5,65 @@ using namespace std;
 
 #define MAX 10
 
+class Stack {
+
+	int top;
+
+public:
+
+	int a[MAX];
+
+	Stack() {top = -1;}
+	bool push(int x);
+	bool isEmpty();
+	int peek();
+	int pop();
+};
+
+bool Stack::push(int x){
+	a[++top] = x;
+
+	cout << x << " was push on the stack" << endl;
+
+	return true;
+}
+
+int Stack::peek(){
+
+	if (top < 0){
+		cout << "Stack is empty" << endl;
+		return 0;
+	}else
+	{
+		int x = a[top];
+		return x;
+	}
+}
+
+int Stack::pop(){
+	int x = a[top--];
+
+	return x;
+}
+
+bool Stack::isEmpty(){
+
+	return (top < 0);
+}
 
 int main(){
 
+	class Stack s;
 
+	s.push(10);
+	s.push(20);
+	cout << s.peek();
+
+	while(!s.isEmpty()){
+		cout << s.peek() << endl;
+
+		s.pop();
+	}
 
 	return 0;
 }
